ae41130aa266d183f025c851b324f76c81f7b133
fpjunior <fpjuniorpx@gmail.com>
2025-07-08 15:38:02 -0300

    feat: Implementar dropdown de estados brasileiros para campo UF

--- frontend/src/app/pacientes/pacientes-form.component.ts
+++ frontend/src/app/pacientes/pacientes-form.component.ts
@@ -18,6 +18,37 @@ export class PacientesFormComponent implements OnInit {
   loading = false;
   apiUrl = environment.apiUrl + '/pacientes';
 
+  // Lista de estados brasileiros
+  estadosBrasileiros = [
+    { sigla: 'AC', nome: 'Acre' },
+    { sigla: 'AL', nome: 'Alagoas' },
+    { sigla: 'AP', nome: 'Amapá' },
+    { sigla: 'AM', nome: 'Amazonas' },
+    { sigla: 'BA', nome: 'Bahia' },
+    { sigla: 'CE', nome: 'Ceará' },
+    { sigla: 'DF', nome: 'Distrito Federal' },
+    { sigla: 'ES', nome: 'Espírito Santo' },
+    { sigla: 'GO', nome: 'Goiás' },
+    { sigla: 'MA', nome: 'Maranhão' },
+    { sigla: 'MT', nome: 'Mato Grosso' },
+    { sigla: 'MS', nome: 'Mato Grosso do Sul' },
+    { sigla: 'MG', nome: 'Minas Gerais' },
+    { sigla: 'PA', nome: 'Pará' },
+    { sigla: 'PB', nome: 'Paraíba' },
+    { sigla: 'PR', nome: 'Paraná' },
+    { sigla: 'PE', nome: 'Pernambuco' },
+    { sigla: 'PI', nome: 'Piauí' },
+    { sigla: 'RJ', nome: 'Rio de Janeiro' },
+    { sigla: 'RN', nome: 'Rio Grande do Norte' },
+    { sigla: 'RS', nome: 'Rio Grande do Sul' },
+    { sigla: 'RO', nome: 'Rondônia' },
+    { sigla: 'RR', nome: 'Roraima' },
+    { sigla: 'SC', nome: 'Santa Catarina' },
+    { sigla: 'SP', nome: 'São Paulo' },
+    { sigla: 'SE', nome: 'Sergipe' },
+    { sigla: 'TO', nome: 'Tocantins' }
+  ];
+
   constructor(private router: Router, private http: HttpClient, private fb: FormBuilder) {
     const nav = this.router.getCurrentNavigation();
     this.pacienteEditando = nav?.extras.state?.['paciente'] || null;
@@ -33,7 +64,7 @@ export class PacientesFormComponent implements OnInit {
       endereco: [''],
       bairro: [''],
       municipio: [''],
-      uf: ['', [Validators.pattern(/^[A-Za-z]{2}$/)]],
+      uf: ['', [Validators.required]],
       cep: ['', [Validators.pattern(/^[0-9]{5}-?[0-9]{3}$/)]],
       acompanhante: [''],
       procedencia: ['']
