450fb90c1b9e549d53f48499214d84c338182d90
fpjunior <fpjuniorpx@gmail.com>
2025-07-16 10:11:02 -0300

    feat(menu): menu global com permissão para acesso à tela de usuários

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: false
 })
 export class AppComponent {
   title = 'frontend';
+  constructor(public authService: AuthService) {}
 }
